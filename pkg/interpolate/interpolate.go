faf3ad61fc2d1c67ca61f698d1b2f4e26f62896c
Xu Xie <Xu.Xie@serko.com>
2019-08-24 15:59:56 +1200

    Implement interpolateString to interpolate string from fields and tgas of Kapacitor point

--- pkg/interpolate/interpolate.go
+++ pkg/interpolate/interpolate.go
@@ -2,9 +2,43 @@ package interpolate
 
 import (
 	"strconv"
+	"strings"
+	"unicode"
 
 	"github.com/influxdata/kapacitor/udf/agent"
 )
+
+func interplolateString(str string, p *agent.Point) (string, error) {
+	// To interpolate string like "Lower {lowerThresh} upper {upperThresh} within {withinSec}s"
+	// with the fields or tags defined in Kapacitor pointer
+
+	var sb strings.Builder
+	var keyName strings.Builder
+	isKeyName := false
+	for _, ch := range str {
+		if ch == '{' {
+			isKeyName = true
+			keyName.Reset()
+		} else if ch == '}' {
+			key := keyName.String()
+			val := getValueByKey(key, p)
+			sb.WriteString(val)
+
+			isKeyName = false
+			keyName.Reset()
+		} else {
+			if isKeyName {
+				if !unicode.IsSpace(ch) {
+					keyName.WriteRune(ch)
+				}
+			} else {
+				sb.WriteRune(ch)
+			}
+		}
+	}
+
+	return sb.String(), nil
+}
 
 func getValueByKey(key string, p *agent.Point) string {
 	if val, ok := p.Tags[key]; ok {
--- pkg/interpolate/interpolate_test.go
+++ pkg/interpolate/interpolate_test.go
@@ -8,6 +8,44 @@ import (
 	"github.com/influxdata/kapacitor/udf/agent"
 )
 
+func TestInterpolateString(t *testing.T) {
+	pnt := getKapacitorPoint()
+
+	for _, tc := range [...]struct {
+		strToInterpolate string
+		pntKapacitor     *agent.Point
+		expected         string
+	}{
+		{"start {fieldIntPos} end", pnt, "start 3 end"},
+		{"start {fieldIntNeg} end", pnt, "start -22 end"},
+		{"start {fieldFloatPos} end", pnt, "start 0.12 end"},
+		{"start {fieldFloatPosRound} end", pnt, "start 0.13 end"},
+		{"start {fieldFloatNeg} end", pnt, "start -0.12 end"},
+		{"start {fieldFloatNegRound} end", pnt, "start -0.13 end"},
+		{"start {fieldStr} end", pnt, "start good end"},
+		{"start {fieldBoolTrue} end", pnt, "start true end"},
+		{"start {fieldBoolFalse} end", pnt, "start false end"},
+		{"start {notExisting} end", pnt, "start  end"},
+		{"start {tag} end", pnt, "start tagValue end"},
+		{"start int field {fieldIntPos}, " +
+			"float field {fieldFloatNegRound}, " +
+			"string field {fieldStr}, " +
+			"tag {tag} and bool field {fieldBoolTrue}",
+			pnt,
+			"start int field 3, " +
+				"float field -0.13, " +
+				"string field good, " +
+				"tag tagValue and bool field true"},
+	} {
+		t.Run(fmt.Sprintf("Interpolate string with Kapacitor point fields and tags"), func(t *testing.T) {
+			actual, _ := interplolateString(tc.strToInterpolate, tc.pntKapacitor)
+			if !reflect.DeepEqual(tc.expected, actual) {
+				t.Errorf("expected %v, actual %v", tc.expected, actual)
+			}
+		})
+	}
+}
+
 func TestGetValueByKey(t *testing.T) {
 	pnt := getKapacitorPoint()
 
